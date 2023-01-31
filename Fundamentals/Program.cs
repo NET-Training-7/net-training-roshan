a83fbe65504cc5a7438d46c4826bc960cc673609
roshantwayana <roshantwayana26@gmail.com>
2023-01-31 09:35:16 +0545

    datatypes

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;// See https://aka.ms/new-console-template for more information
+Console.Write("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+//datatypes on c#
+//static(c#, java) vs dynamic(js,python)
+
+//oop(c#)
+//functional languages(haskel,closure,f#) vs procedural languages(c)
