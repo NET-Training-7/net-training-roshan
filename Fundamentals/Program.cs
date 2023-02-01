704058648c8773a464d1740044e8d71e1748a1bd
roshantwayana <roshantwayana26@gmail.com>
2023-02-01 09:29:41 +0545

    Learn about datatypes

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;// See https://aka.ms/new-console-template for more information
-Console.Write("Hello, World!");
-Console.WriteLine("Hello, World!");
+class ABC
+{
+    public static void Main()
+    {
+        Test test = new();
+        test.Foo();
+    }
+}
 
-//datatypes on c#
-//static(c#, java) vs dynamic(js,python)
 
-//oop(c#)
-//functional languages(haskel,closure,f#) vs procedural languages(c)
