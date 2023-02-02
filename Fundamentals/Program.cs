3a8d697b72e341bc01a4d34218e3d7fb9ee7f3d8
roshantwayana <roshantwayana26@gmail.com>
2023-02-02 09:35:00 +0545

    assignment completion

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,26 +1,22 @@
 ﻿using System;// See https://aka.ms/new-console-template for more information
- class ABC
-{
-     public static void Main()
-    {
-        Test test = new();
-        test.Foo();
-    }
-}
+//  class ABC
+// {
+//      public static void Main()
+//     {
+//         Test test = new();
+//         test.Foo();
+//     }
+// }
 
 
-class BCD
-{
-    internal void Assign()
-{
+
+
     Animal animal1 = new();
     var animal2 = new Animal();
     animal1.Name = "Lion";
     animal1.Species = "Panthera leo";
     animal1.Color = "Golden";
     animal1.Age = 5;
-    
+    animal1.DisplayDetails();
 
-}
-
-}
+ 
