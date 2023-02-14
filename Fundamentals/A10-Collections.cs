3e87174a713b4ea7f519f07966b60b66549e579f
roshantwayana <roshantwayana26@gmail.com>
2023-02-14 08:12:10 +0545

    collections

---
+++ Fundamentals/A10-Collections.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+class Collections
+{
+    void LearnDictionary()
+    {
+        Dictionary<string,  int> districtPopulation = new();
+        districtPopulation.Add("kathmandu", 200000);
+        districtPopulation.Add("lalitpur", 100000);
+        districtPopulation.Add("bhaktapur", 50000);
+        districtPopulation.Add("pokhara", 20000);
+    }
+}
