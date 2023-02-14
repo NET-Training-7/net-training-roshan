13a7891be1e88e7445fd8f8a1460392428283e9b
roshantwayana <roshantwayana26@gmail.com>
2023-02-14 08:36:22 +0545

    linq

--- Fundamentals/A11- LINQ.cs
+++ Fundamentals/A11- LINQ.cs
@@ -6,6 +6,15 @@ class LINQ
     public void LearnToQuery()
     {
         var oddNumbers = scores.Where(s => s % 2 != 0);
+        
+        var cubesBetween50and70 = scores.Where(s => s >= 50 && s<=70).Select(s => s * s * s);
+
+        cubesBetween50and70 = from s in scores
+                              where s >= 50 && s<= 70
+                              select s * s * s;
+
+        var squareRoot = scores.Where(s => s % 10 == 0).Select(s => Math.Sqrt(s));                     
+
         foreach(var num in oddNumbers)
         {
             Console.Write($"{num} ");
