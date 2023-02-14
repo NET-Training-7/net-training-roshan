8a8af7b429be51a0f04a69d1ff2e2361a18e1d61
roshantwayana <roshantwayana26@gmail.com>
2023-02-14 21:12:18 +0545

    learn generics

---
+++ Fundamentals/A9-Generics.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Generics<Y>
+{
+    public void Print<X>(X x)
+    {
+        Console.WriteLine($"Printing given argument: {x}");
+    }    
+
+    public void Greet(Y message)
+    {
+        Console.WriteLine(message);
+    }
+}
