9ecbea896d3859e962201a163f25a5b8f046762a
roshantwayana <roshantwayana26@gmail.com>
2023-02-17 08:40:23 +0545

    linq

--- Fundamentals/A11- LINQ.cs
+++ Fundamentals/A11- LINQ.cs
@@ -1,12 +1,31 @@
 // LINQ - Language Integrated Query
 // Technique to query collections (array, list, dictionaries etc)
 using System;
+using System.Collections.Generic;
 using System.Linq;
 class LINQ
 {
     int[] scores = { 3, 4, 5, 6, 7, 8, 9, 10, 55, 56, 57, 58 };
 
     string[] names = { "Elon Musk", "Balen Shah", "Leo Messi", "Neymar Jr" };
+
+    List<Person> people = new()
+    {
+        new Person(){EducationDegreeId=1, Natioanality="Nepal", Name = "Ram Ji", Dob = new DateTime(1998, 12, 13)},
+        new Person(){EducationDegreeId=2, Natioanality="China", Name = "Ajaya", Dob = new DateTime(2010, 1, 13)},
+        new Person(){EducationDegreeId=3, Natioanality="India", Name = "Krishna", Dob = new DateTime(1988, 2, 23)},
+        new Person(){EducationDegreeId=3, Natioanality="USA", Name = "Anirudra", Dob = new DateTime(1978, 3, 1)},
+        new Person(){EducationDegreeId=1, Natioanality="India", Name = "Kishan", Dob = new DateTime(1990, 4, 13)},
+        new Person(){EducationDegreeId=2, Natioanality="Nepal", Name = "Roshan", Dob = new DateTime(2008, 10, 29)},
+    };
+
+    List<EducationDegree> degrees = new()
+    {
+        new() { Id = 1, Major = "Science", Title = "M.Sc.", University = "TU"},
+        new() { Id = 2, Major = "Arts", Title = "M.A.", University = "KU"},
+        new() { Id = 3, Major = "Education", Title = "M.Ed.", University = "TU"}
+    };
+   
     public void LearnToQuery()
     {
         // Q1. Find all even numbers from scores
@@ -54,5 +73,22 @@ class LINQ
         {
             Console.Write($"{num} ");
         }
+    }
+
+    public void LearnToQueryComplexCollections()
+    {
+        //Find all people who can vote
+        // var votelist = people.Where(p => p.Age >= 18);
+        // // find names of people who can vote
+        // var votelistAndNames = people.Where(p => p.Age >= 18).Select(p => p.Name);
+
+        // votelistAndNames = from p in people
+        //                 where() >= 18
+        //                 select p.Name;
+       // find all nepalese people who born after .Net released
+        var nepaleseAfterDotnet = from p in people
+                                  where p.Natioanality == "Nepal" && p.Dob.Year > 2002
+                                  select p;
+
     }
 }
--- Fundamentals/A2-ClassesAndObjects/Constructor.cs
+++ Fundamentals/A2-ClassesAndObjects/Constructor.cs
@@ -1,33 +1,98 @@
 using System;
+
 class Person
 {
-   internal string name;
-    internal byte age;
+    internal string Name { get; set; }
+    internal DateTime Dob { get; set; }
+    internal string Natioanality { get; set; }
+    public int EducationDegreeId { get; set; }
 
-    public Person() //Default parameterless constructor(doesnot have return type)
+    public Person()     // Default parameterless contructor
     {
     }
 
-    public Person(string n, byte a)
+    public Person(string n, DateTime a)
     {
-        name = n;
-        age = a;
+        Name = n;
+        Dob = a;
     }
+
+    ~Person()
+    {
+
+    }
+}
+
+class EducationDegree
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Major { get; set; }
+    public string University { get; set; }
 }
 
 class Test2
 {
- internal void T()
- {
-    Person p1 = new();   // var p1 = new Person();
-    p1.age = 23;
-    p1.name = "Ram";
-    Person p2 = new("Kishan", 67);
-    Console.WriteLine($"{p1.name}, {p1.age}");
-    Console.WriteLine($"{p2.name}, {p2.age}");
+    void T()
+    {
+        var p1 = new Person();
+        // p1.Age = 23;
+        p1.Name = "Ram";
+
+        var p2 = new Person("Kishan", DateTime.Now);
+        var p3 = new Person("Kishan", DateTime.Now);
+    }
+}
+// using System;
+
+// class Person
+// {
+//     internal string Name { get; set; }
+//     internal DateTime Dob { get; set; }
+//     internal string Natioanality { get; set; }
+
+//     public Person()     // Default parameterless contructor
+//     {
+//     }
+
+//     public Person(string n, DateTime a)
+//     {
+//         Name = n;
+//         Dob = a;
+//     }
+
+//     ~Person()
+//     {
+
+//     }
+// }
+
+// class Test2
+// {
+//     void T()
+//     {
+//         var p1 = new Person();
+//         // p1.Age = 23;
+//         p1.Name = "Ram";
+
+//         var p2 = new Person("Kishan", DateTime.Now);
+//         var p3 = new Person("Kishan", DateTime.Now);
+//     }
+// }
+
+// class Test2
+// {
+//  internal void T()
+//  {
+//     Person p1 = new();   // var p1 = new Person();
+//     p1.Age = 23;
+//     p1.Name = "Ram";
+//     Person p2 = new("Kishan", 67);
+//     Console.WriteLine($"{p1.Name}, {p1.Age}");
+//     Console.WriteLine($"{p2.Name}, {p2.Age}");
 
   
-  }
+//   }
   
   
-}
+// }
