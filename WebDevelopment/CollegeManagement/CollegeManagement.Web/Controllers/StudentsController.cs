1cc285824445b9da91e5a422d24614152861b52e
roshantwayana <roshantwayana26@gmail.com>
2023-03-03 09:21:34 +0545

    project

---
+++ WebDevelopment/CollegeManagement/CollegeManagement.Web/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeManagement.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Render all students 
+            return View();
+        }
+    }
+}
